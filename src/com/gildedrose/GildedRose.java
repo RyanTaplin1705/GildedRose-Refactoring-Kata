0fb1119b2bda13ca8f4d3159b9f85ff62632c1f9
Ryan Taplin <ryan.taplin170596@gmail.com>
2017-07-26 21:32:09 +0100

    Removed all comments in code speifying extra requirements. Spotted error in code or missing bad requirement that allows AgedBrie to increase in quality by 2 when SellIn is zero or less.

--- src/com/gildedrose/GildedRose.java
+++ src/com/gildedrose/GildedRose.java
@@ -13,15 +13,12 @@ class GildedRose {
 
         //for all items
         for (int i = 0; i < items.length; i++) {
-            //if item is Regular and quality > 0 then decrement quality by 1
             if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
-            // if item is AgedBrie or BackStagePass and quality is less than 50 increase quality by 1
-            // if item is BackstagePass and sellIn is 10 or less then increment quality by 1 and if SellIn is 5 or less then increment quality by 1 again
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
@@ -42,14 +39,9 @@ class GildedRose {
                 }
             }
 
-            // if item is not Sulfuras decrease sellIn by 1
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
-
-            // if Sellin is less than zero backstage pass has 0 quality;
-            // if Sellin is less than zero and not Sulfuras decrease quality by 1
-            // if Sellin is less than zero AgedBrie increase by 1 quality (but not more than 50)
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
